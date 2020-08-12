7bcaafaf7ebb2eef72ce3b2ef9e53c4cd24e9723
mattchley <mattchley@gmail.com>
2020-08-12 13:03:40 -0700

    basic cli side prompt for length of word

---
+++ basic_init.py
@@ -0,0 +1,5 @@
+num = input("How long of a word would you like? \n(i.e. 4 letters long)\n")
+space = "_ "
+print_spaces = print("There are 26 leters to file in these " + (space * int(num)) + "spaces")
+
+# starts the prompt for how  long the word will be
