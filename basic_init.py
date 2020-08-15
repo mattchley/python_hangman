58df698e9c18ac28b9e4c2936fe6cb80fc4c4d3a
mattchley <mattchley@gmail.com>
2020-08-14 17:54:17 -0700

    get request from API works

--- basic_init.py
+++ basic_init.py
@@ -1,23 +1,41 @@
 import string
+import sys
+import requests
+
+chosen_letters = []
+letter_choices = list(string.ascii_lowercase)
+
+url = "https://wordsapiv1.p.rapidapi.com/words/"
+
+querystring = {"random": "true"}
+
+headers = {
+        'x-rapidapi-host': "wordsapiv1.p.rapidapi.com",
+        'x-rapidapi-key': 
+    }
+
+response = requests.request("GET", url, headers=headers, params=querystring)
+res = response.json()
+secret_word = res['word']
+
+
 
 
 def start():
+    print(secret_word)
     print("Ready to play a game of hangman???")
     print("The game is simple. You have a limited number of guesses to find all the letters in a word of some length.")
-    print("You will decide the length.")
-    if input("Do you wish to continue??? [Y/N] ") == "Y" or "y":
-        length = input("How long should the word be?")
-        print("I will find a " + length + " letter word for you")
+    # print("You will decide the length.")
+    if input("Do you wish to continue??? [Y/N] ") == "Y":
+        # length = input("How long should the word be?")
+        # print("I will find a " + length + " letter word for you")
         play()
     else:
-        print("Guess we will play another time")
+        print("Guess we will play another time.\n See you later!!!")
+        sys.exit()
 
 
 def play():
-    secret_word = "progress"
-    chosen_letters = []
-    letter_choices = list(string.ascii_lowercase)
-
     x = 0
     while x < 8:
         chosen = input("What letter do you want to choose?\n" + str(letter_choices) + "\n")
